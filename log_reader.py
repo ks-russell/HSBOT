4b1d6feb00fbbfee59cbdc7066dc3736992bb19f
ks-russell <sobaman14@gmail.com>
2015-06-18 23:48:33 -0700

    added automatic log finding (just run script before game start)

--- log_reader.py
+++ log_reader.py
@@ -1,9 +1,28 @@
+import os
+import datetime
+import sys
 
-thefile = open('/Users/krussell/Library/Preferences/Blizzard/Hearthstone/Logs/hearthstone_2015_06_17_22_48_12.log')
+log_location = '/Users/krussell/Library/Preferences/Blizzard/Hearthstone/Logs/'
+newest_file = None
+newest_file_time = 0
+
+for file in os.listdir(log_location):
+	t = os.path.getctime(log_location + file)
+	time = datetime.datetime.fromtimestamp(t)
+	timeStr = time.strftime('%Y%m%d%X')
+	condensed_timeStr = timeStr[0:9] + timeStr[11:13] + timeStr[14:]
+	if int(condensed_timeStr) > newest_file_time:
+		newest_file_time = int(condensed_timeStr)
+		newest_file = file
+
+thefile = open(log_location + newest_file)
 
 while True:
 	ret1 = thefile.read()
 	str = thefile.seek(0)
+	if str == None:
+		print('Hearthstone game not found.')
+		sys.exit()
 	ret2 = thefile.read()
 	if len(ret1) > 1 & str == 0:
 		print(ret1)
